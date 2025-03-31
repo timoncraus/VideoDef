8be1f6a43ef70f9e4644ac5bfdf3ecec8c9e8f02
Timofey Gorshkov <tima.gorshkov@list.ru>
2025-04-01 00:18:21 +0500

    Более безопасное принятие сервером сообщение от пользователя
    Чтобы пользователь не мог даже теоретически отправить сообщение под чужим именем

--- videodef/chat/consumers.py
+++ videodef/chat/consumers.py
@@ -6,86 +6,48 @@ from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Получаем chat_id из URL
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
-        
-        # Название комнаты
         self.room_group_name = f'chat_{self.chat_id}'
 
-        # Присоединяемся к группе WebSocket
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
+        # Проверяем, авторизован ли пользователь
+        self.user = self.scope["user"]
+        if self.user.is_anonymous:
+            await self.close()  # Закрываем соединение, если пользователь не авторизован
+        else:
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            await self.accept()
 
     async def disconnect(self, close_code):
-        # Отключение от группы
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
-        # Обрабатываем сообщение от клиента
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        user_id = text_data_json['user_id']
-        sender = await self.get_user(user_id)
 
-        # Сохраняем сообщение в базу данных
+        # Используем текущего пользователя из self.scope["user"]
+        sender = self.user
+
+        # Сохраняем сообщение
         await self.save_message(sender, message)
 
-        # Отправляем сообщение в группу WebSocket
+        # Отправляем сообщение всем в чате
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'user_id': user_id
+                'user_id': sender.unique_id,
             }
         )
 
-
     async def chat_message(self, event):
-        # Получаем сообщение из события
-        message = event['message']
-        user_id = event['user_id']
-
-        # Отправляем сообщение клиенту
         await self.send(text_data=json.dumps({
-            'message': message,
-            'user_id': user_id
+            'message': event['message'],
+            'user_id': event['user_id'],
         }))
-
-    #async def chat_message(self, event):
-    #    # Получаем сообщение из события
-    #    message = event['message']
-    #    sender_id = event['sender']
-#
-    #    # Получаем профиль отправителя асинхронно
-    #    sender = await self.get_user(sender_id)
-    #    sender_profile = await self.get_user_profile(sender)
-    #    print(sender_profile)
-#
-    #    # Отправляем сообщение клиенту
-    #    await self.send(text_data=json.dumps({
-    #        'message': message,
-    #        'sender_info': str(sender_profile)
-    #    }))
-
-    @database_sync_to_async
-    def get_user(self, user_id):
-        return User.objects.get(unique_id=user_id)
-
-    #@database_sync_to_async
-    #def get_user_profile(self, user):
-    #    return user.profile  # Возвращаем профиль пользователя
-        
 
     @database_sync_to_async
     def save_message(self, sender, message):
         """ Сохраняем сообщение в базу данных """
-        chat = SmallChat.objects.get(id=self.chat_id)  # Получаем чат
-        Message.objects.create(chat=chat, sender=sender, content=message)  # Создаём сообщение
+        chat = SmallChat.objects.get(id=self.chat_id)
+        Message.objects.create(chat=chat, sender=sender, content=message)
