d1f27573758c0dbed5f28895f1c1d4f0d34bb82b
Ivan Salikov <salikovivan7@gmail.com>
2025-04-04 17:59:02 +0500

    К странице game_main.html добавлены стили css. Добавлены тестовые данные в views.py (приложения game) для теста отображения элементов страницы

--- videodef/game/views.py
+++ videodef/game/views.py
@@ -6,5 +6,6 @@ def games(request):
         {"title": "Игра 2", "description": "Описание игры 2", "image": "images/Game_Image.png"},
         {"title": "Игра 3", "description": "Описание игры 3", "image": "images/Game_Image.png"},
         {"title": "Игра 4", "description": "Описание игры 4", "image": "images/Game_Image.png"},
+        {"title": "Игра 5", "description": "Описание игры 5", "image": "images/Game_Image.png"},
     ]
     return render(request, "game/game_main.html", {"games": dummy_games})
