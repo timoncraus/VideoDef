0b91c55a10ce01a4527b1c981267edec640e4fb5
Ivan Salikov <salikovivan7@gmail.com>
2025-04-04 17:39:02 +0500

    Выполнена верстка главной страницы раздела игры

--- videodef/game/views.py
+++ videodef/game/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def games(request):
-    return render(request, "games.html")
+    dummy_games = [
+        {"title": "Игра 1", "description": "Описание игры 1", "image": "images/Game_Image.png"},
+        {"title": "Игра 2", "description": "Описание игры 2", "image": "images/Game_Image.png"},
+        {"title": "Игра 3", "description": "Описание игры 3", "image": "images/Game_Image.png"},
+        {"title": "Игра 4", "description": "Описание игры 4", "image": "images/Game_Image.png"},
+    ]
+    return render(request, "game/game_main.html", {"games": dummy_games})
