e906a683935f917bf53d4e2645c77471f550d3f6
Ivan Salikov <salikovivan7@gmail.com>
2025-04-06 13:28:27 +0500

    Добавлена временная переадресация на страницу игры при нажатии на её карточку

--- videodef/game/views.py
+++ videodef/game/views.py
@@ -4,11 +4,16 @@ def games(request):
     dummy_games = [
         {"title": "Пазлы", "description": "Увлекательная игра, которая поможет развить внимание,\
           логику и пространственное восприятие. В этой игре вам предстоит собирать изображения, \
-         разделенные на кусочки, и восстанавливать их в правильном порядке.", "image": "images/Game_Image.png"},
-        {"title": "Игра 2", "description": "Описание игры 2", "image": "images/Game_Image.png"},
-        {"title": "Игра 3", "description": "Описание игры 3", "image": "images/Game_Image.png"},
-        {"title": "Игра 4", "description": "Описание игры 4", "image": "images/Game_Image.png"},
-        {"title": "Игра 5", "description": "Описание игры 5", "image": "images/Game_Image.png"},
+         разделенные на кусочки, и восстанавливать их в правильном порядке.", "image": "images/Game_Image.png", \
+            "url": "puzzle_game"},
+        {"title": "Игра 2", "description": "Описание игры 2", "image": "images/Game_Image.png",\
+         "url": "#"},
+        {"title": "Игра 3", "description": "Описание игры 3", "image": "images/Game_Image.png",\
+         "url": "#"},
+        {"title": "Игра 4", "description": "Описание игры 4", "image": "images/Game_Image.png",\
+         "url": "#"},
+        {"title": "Игра 5", "description": "Описание игры 5", "image": "images/Game_Image.png",\
+         "url": "#"},
     ]
     return render(request, "game/game_main.html", {"games": dummy_games})
 
