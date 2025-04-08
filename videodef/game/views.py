f3330e7481abc6da8df26725279248126b894b24
Ivan Salikov <salikovivan7@gmail.com>
2025-04-08 20:51:24 +0500

    Сделена временная переадресация на доску при нажатиии на её карточку в разделе игры

--- videodef/game/views.py
+++ videodef/game/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 def games(request):
     dummy_games = [
+        {"title": "Интерактивная доска", "description": "Интерактивная доска для совместного рисования и работы с изображениями \
+          в реальном времени. Поддерживает многопользовательский режим, перетаскивание и масштабирование картинок.", \
+          "image": "images/board.png",\
+         "url": "whiteboard"},
         {"title": "Пазлы", "description": "Увлекательная игра, которая поможет развить внимание,\
           логику и пространственное восприятие. В этой игре вам предстоит собирать изображения, \
-         разделенные на кусочки, и восстанавливать их в правильном порядке.", "image": "images/Game_Image.png", \
+          разделенные на кусочки, и восстанавливать их в правильном порядке.", "image": "images/Game_Image.png", \
             "url": "puzzle_game"},
-        {"title": "Игра 2", "description": "Описание игры 2", "image": "images/Game_Image.png",\
-         "url": "#"},
         {"title": "Игра 3", "description": "Описание игры 3", "image": "images/Game_Image.png",\
          "url": "#"},
         {"title": "Игра 4", "description": "Описание игры 4", "image": "images/Game_Image.png",\
