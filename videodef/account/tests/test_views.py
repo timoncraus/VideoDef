83650796e96f042ea7fae05a91f76c8feb10765b
Timofey Gorshkov <tima.gorshkov@list.ru>
2026-06-18 18:06:34 +0500

    Исправление тестов account

--- videodef/account/tests/test_views.py
+++ videodef/account/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from datetime import datetime
 
 from account.models import Role, Gender, Profile
 
@@ -27,6 +28,7 @@ class ViewTests(TestCase):
             date_birth="2000-01-01",
             role=self.role,
             gender=self.gender,
+            max_search_distance=10,  # Добавляем значение по умолчанию
         )
         self.user.save()
 
@@ -70,6 +72,7 @@ class ViewTests(TestCase):
         self.assertTemplateUsed(response, "account/register_form.html")
 
     def test_register_view_post_valid(self):
+        # Правильный формат даты: YYYY-MM-DD
         response = self.client.post(
             reverse("account:register"),
             {
@@ -81,9 +84,10 @@ class ViewTests(TestCase):
                 "first_name": "Новый",
                 "last_name": "Пользователь",
                 "patronymic": "Тестович",
-                "date_birth": "01.01.2000",
+                "date_birth": "2000-01-01",  # Исправлен формат
                 "role": self.role.id,
                 "gender": self.gender.id,
+                "max_search_distance": 10,  # Добавляем обязательное поле
             },
             follow=True,
         )
@@ -116,9 +120,30 @@ class ViewTests(TestCase):
         self.assertContains(response, "Вы вышли из системы!")
 
     def test_view_other_user(self):
+        # Создаем второго пользователя для просмотра
+        other_user = User.objects.create_user(
+            username="otheruser",
+            email="other@example.com",
+            password="password123",
+        )
+        other_user.profile = Profile.objects.create(
+            first_name="Другой",
+            last_name="Пользователь",
+            patronymic="Тестович",
+            date_birth="2000-01-01",
+            role=self.role,
+            gender=self.gender,
+            max_search_distance=10,
+        )
+        other_user.save()
+        
+        # Логинимся как первый пользователь
+        self.client.login(username="testuser", password="password123")
+        
+        # Просматриваем профиль другого пользователя
         response = self.client.get(
-            reverse("account:view_other_user", args=[self.user.unique_id])
+            reverse("account:view_other_user", args=[other_user.unique_id])
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "account/user_view.html")
-        self.assertContains(response, self.user.username)
+        self.assertContains(response, other_user.username)
